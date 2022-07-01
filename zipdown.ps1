91a6dd0b228a697f9f54d4f2fd83e1049304d761
Sahindu Gayanuka <65648007+SahiDemon@users.noreply.github.com>
2022-07-01 09:54:43 +0530

    Add files via upload

---
+++ zipdown.ps1
@@ -0,0 +1,19 @@
+﻿#github url to download zip file
+#Assign zip file url to local variable
+
+$Url = "https://github.com/SahiDemon/proxydata/archive/refs/heads/main.zip"
+
+$DownloadZipFile = "D:\ZipFile\" + $(Split-Path -Path $Url -Leaf)
+
+$ExtractPath = "D:\UnZipFiles\"
+
+Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+
+$ExtractShell = New-Object -ComObject Shell.Application 
+
+$ExtractFiles = $ExtractShell.Namespace($DownloadZipFile).Items() 
+
+$ExtractShell.NameSpace($ExtractPath).CopyHere($ExtractFiles) 
+Start-Process $ExtractPath
+
+
