e128156f879e13a22be1dc3dec4b54379638458f
Sahindu Gayanuka <65648007+SahiDemon@users.noreply.github.com>
2022-11-20 17:54:11 +0530

    Add files via upload

---
+++ v2raydown.ps1
@@ -0,0 +1,71 @@
+# Copyright 2022 SahiDemon. 
+
+param (
+  [string] $version
+)
+
+$PSMinVersion = 3
+
+if ($v) {
+    $version = $v
+}
+
+# Helper functions for pretty terminal output.
+function Write-Part ([string] $Text) {
+  Write-Host $Text -NoNewline
+}
+
+function Write-Emphasized ([string] $Text) {
+  Write-Host $Text -NoNewLine -ForegroundColor "Cyan"
+}
+
+function Write-Done {
+  Write-Host " > " -NoNewline
+  Write-Host "OK" -ForegroundColor "Green"
+}
+
+if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
+  $ErrorActionPreference = "Stop"
+
+  # Enable TLS 1.2 since it is required for connections to GitHub.
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+
+
+  # Create ~\proxy directory if it doesn't already exist
+  $sp_dir = "${HOME}\ProxyManager\v2rayN"
+  if (-not (Test-Path $sp_dir)) {
+    Write-Part "MAKING FOLDER  "; Write-Emphasized $sp_dir
+    New-Item -Path $sp_dir -ItemType Directory | Out-Null
+    Write-Done
+  }
+
+  # Download release.
+  $zip_file = "${sp_dir}\v2rayN.zip"
+  $download_uri = "https://github.com/2dust/v2rayN/releases/download/5.38/v2rayN-Core.zip"
+  Write-Part "DOWNLOADING    "; Write-Emphasized $download_uri
+  Invoke-WebRequest -Uri $download_uri -UseBasicParsing -OutFile $zip_file
+  Write-Done
+
+  
+  # Extract zip and assets from .zip file.
+  Write-Part "EXTRACTING     "; Write-Emphasized $zip_file
+  Write-Part " into "; Write-Emphasized ${sp_dir};
+  # Using -Force to overwrite spicetify.exe and assets if it already exists
+  Expand-Archive -Path $zip_file -DestinationPath $sp_dir -Force
+  Write-Done
+
+    # Remove .zip file.
+  Write-Part "REMOVING       "; Write-Emphasized $zip_file
+  Remove-Item -Path $zip_file
+  Write-Done
+
+
+  Write-Done "`n v2ray files download sucessfull."
+  Write-Part "Run "; Write-Emphasized "ProxyManager By SahiDemon"; Write-Host " to get started.`n"
+}
+else {
+  Write-Part "`nYour Powershell version is lesser than "; Write-Emphasized "$PSMinVersion";
+  Write-Part "`nPlease, update your Powershell downloading the "; Write-Emphasized "'Windows Management Framework'"; Write-Part " greater than "; Write-Emphasized "$PSMinVersion"
+}
+
